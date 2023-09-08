a3c6cdb6c56a3798a4e4e2e2c9b08599f4563906
cedric karungu <ckarungu921@gmail.com>
2023-09-08 14:48:23 +0200

    feat: add projects routes

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ProjectService } from './project.service';
 
-@Controller('project')
-export class ProjectController {}
+@Controller('projects')
+export class ProjectController {
+  constructor(private projectService: ProjectService) {}
+
+  @Get()
+  getAllProjects(@Query('q') queryString: string) {
+    return this.projectService.getProjectsData(queryString);
+  }
+
+  @Get('/user:id')
+  getUserProjects(@Param('id') userId: string) {
+    return this.projectService.getUserProjects(userId);
+  }
+
+  @Get(':id')
+  getProjectDetails(@Param('id') id: string) {
+    return this.projectService.getProjectDetails(id);
+  }
+
+  @Post()
+  createProject(@Body() dto: any) {
+    return this.projectService.createProject('1', dto);
+  }
+}
