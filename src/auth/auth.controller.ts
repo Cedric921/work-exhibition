70688869ed45a312a184d2939fd7a107214a432a
cedric karungu <ckarungu921@gmail.com>
2023-09-08 16:25:01 +0200

    feat: auth chore

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDTO } from './dto';
+import { CreateUserDTO } from 'src/user/dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  register(@Body() dto: any) {
+  register(@Body() dto: CreateUserDTO) {
     return this.authService.register(dto);
   }
 
   @Post('login')
-  login(@Body() dto: any) {
+  login(@Body() dto: LoginDTO) {
     return this.authService.login(dto);
   }
 }
