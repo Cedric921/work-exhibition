7e9f1efc3ebe7f6eed0a69d98f37e24dbac3009a
cedric karungu <ckarungu921@gmail.com>
2023-09-10 17:53:45 +0200

    feat: connect upload user avatar file

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { AwsUploadFileModule } from 'src/aws-upload-file/aws-upload-file.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), AwsUploadFileModule],
   controllers: [UserController],
   providers: [UserService, JwtStrategy],
 })
